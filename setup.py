88ec0e23d8acba45c79dfce3c17de4b4c115fc7f
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2014-01-16 23:16:17 +1100

    dependencies + version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = os.path.join(curdir, 'README.rst')
 
 setup(
     name='grind',
-    version='0.1.0',
+    version='0.1.1',
     description='delegate tasks to EC2 spot instances',
     long_description=open(readme).read(),
     author='Eugene Van den Bulke',
@@ -15,10 +15,11 @@ setup(
     url='http://github.com/3kwa/grind',
     py_modules=['grind'],
     entry_points = {'console_scripts': ['grind = grind:main']},
+    install_requires = ['boto', 'paramiko'],
     license='BSD',
     classifiers=[
-    'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7'
         ],
      )
